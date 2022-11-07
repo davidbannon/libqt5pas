aa8e08f5c62cf8b8198a5c00496d66d1f8757570
David Bannon <tomboy-ng@bannons.id.au>
2022-11-07 20:03:12 +1100

    Can now make armhf, ie 32bit

--- cbindings/package/rpm-package.bash
+++ cbindings/package/rpm-package.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-# Note : must be run as root !
+# Note : must be run as root or fakeroot 
 
 # Script to build rpms from the pre existing libqt5pas libraries
 # A straight call to Alien is not suitable because it upsets at least
@@ -9,6 +9,7 @@
 # David Bannon, 2020/03/21, LGPL with exception, see associated COPYING.TXT
 
 VERSION=`cat ../version`
+ARCH=`uname -m`
 
 MAJORVER=`echo $VERSION | awk -F"." '{ printf $1 }'`		# for 1.2.8,  MAJORVER=1
 MINORVER=`echo $VERSION | awk -F"." '{ printf $2"."$3 }'`	# for 1.2.8,  MINORVER=2.8
@@ -20,18 +21,22 @@ function MakeRPM () {
 	echo "============ Processing $PRODUCT$1 ==============="
 	RDIR="$PRODUCT$1-$MINORVER"
 	rm -Rf "$RDIR"
-	alien -r -g -v "$PRODUCT$1_$2-0_amd64.deb"
+	alien -r -g -v "$PRODUCT$1_$2-0_""$ARCH"".deb"
 	# Alien inserts requests the package create /, /usr/lib and /usr/bin and
 	# the os does not apprieciate that, not surprisingly.
 	sed -i 's#%dir "/"##' "$RDIR"/"$RDIR"-2.spec
 	sed -i 's#%dir "/usr/bin/"##' "$RDIR"/"$RDIR"-2.spec
 	sed -i 's#%dir "/usr/lib/"##' "$RDIR"/"$RDIR"-2.spec
 	cd "$RDIR"
-	rpmbuild --target x86_64 --buildroot "$CURRENT/$RDIR" -bb "$RDIR"-2.spec
+	rpmbuild --target "$ARCH" --buildroot "$CURRENT/$RDIR" -bb "$RDIR"-2.spec
 	cd ..
-	chown $SUDO_USER *.rpm
+	# chown $SUDO_USER *.rpm
 }
 
+# this is what mount -m reports, intel and arm64 are ok
+if [ "$ARCH" == "armv7l" ]; then
+	ARCH="armhf"
+fi
 MakeRPM "$MAJORVER" "$MINORVER"        
 MakeRPM "-dev" "$MINORVER"          
 
