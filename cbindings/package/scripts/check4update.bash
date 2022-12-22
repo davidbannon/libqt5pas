d3643db931e7360023c1b7e0caf70358d5888a5f
David Bannon <dbannon@internode.on.net>
2022-12-22 13:26:04 +1100

    playing workflow 7

--- cbindings/package/scripts/check4update.bash
+++ cbindings/package/scripts/check4update.bash
@@ -1,15 +1,21 @@
 #!/usr/bin/bash
 
-REMOTE_pro=https://gitlab.com/freepascal.org/lazarus/lazarus/-/raw/main/lcl/interfaces/qt5/cbindings/Qt5Pas.pro
+QTV=5    # or 5 or 6
+
+REMOTE_pro="https://gitlab.com/freepascal.org/lazarus/lazarus/-/raw/main/lcl/interfaces/qt""$QVT""/cbindings/Qt""$QTV""Pas.pro"
+REMOTEfile="Qt""$QVT""Pas.pro"
+LOCALfile="libQtPas_src/cbindings/Qt""$QTV""Pas.pro"
+
+
 # https://raw.githubusercontent.com/davidbannon/libqt6pas/master/cbindings/Qt6Pas.pro
 
 wget "$REMOTE_pro"
-REMOTEqt=`grep VER_PAT Qt5Pas.pro`
-LOCALqt=`grep VER_PAT libQtPas_src/cbindings/Qt5Pas.pro`
-if [ "$REMOTEqt" == "$LOCALqt" ]; then
+REMOTEpat=`grep VER_PAT "$REMOTEfile"`
+LOCALpat=`grep VER_PAT "$LOCALFILE"`
+if [ "$REMOTEpat" == "$LOCALpat" ]; then
     echo "no change" >> result.text
 else
-    echo "$REMOTEqt" >> result.text
+    echo "$REMOTEpat" >> result.text
 fi
 cat result.text
 
